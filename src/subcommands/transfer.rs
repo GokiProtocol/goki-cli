7d757f05fe24364dbcb9663b927e9e3fd99d2079
Goki Rajesh <goki@goki.so>
2022-04-23 23:37:22 +0530

    allow transfers on mainnet

--- src/subcommands/transfer.rs
+++ src/subcommands/transfer.rs
@@ -1,20 +1,13 @@
 use crate::workspace::Workspace;
 use anchor_client::Cluster;
-use anyhow::{format_err, Result};
+use anyhow::Result;
 
 pub fn process(workspace: &Workspace, cluster: Cluster, to_raw: &str, amount: &str) -> Result<()> {
-    if cluster == Cluster::Mainnet {
-        return Err(format_err!("cannot request an airdrop from mainnet"));
-    }
-
     let ctx = workspace.new_upgrader_context(&cluster);
-
     let to = match to_raw {
         "deployer" => ctx.get_deployer_kp_path().display().to_string(),
         _ => to_raw.to_string(),
     };
-
     ctx.exec_args(&["transfer", &to, amount])?;
-
     Ok(())
 }
--- src/utils.rs
+++ src/utils.rs
@@ -106,3 +106,15 @@ pub fn sha256_digest<R: Read>(reader: &mut R) -> Result<(u64, String)> {
     let hash_bytes = hasher.finalize();
     Ok((num_bytes, HEXLOWER.encode(hash_bytes.as_ref())))
 }
+
+pub fn pause(message: &str) {
+    let mut stdin = io::stdin();
+    let mut stdout = io::stdout();
+
+    // We want the cursor to stay at the end of the line, so we print without a newline and flush manually.
+    write!(stdout, "{}", message).unwrap();
+    stdout.flush().unwrap();
+
+    // Read a single byte and discard
+    let _ = stdin.read(&mut [0u8]).unwrap();
+}
